10803a8998e710102c9fc3e106ef4df0133b91e2
MelvolaMonique <monique.lima10@etec.gov.br>
2023-09-27 08:30:56 -0300

    primeira parte terminada
    parte de filtros e header terminada, fazer um estilo mais bonito

--- src/pages/Estante/index.tsx
+++ src/pages/Estante/index.tsx
@@ -1,13 +1,31 @@
 import styles from './Estante.module.scss';
 import Logo from 'assets/logo.svg';
+import Buscador from './Buscador';
+import { useState } from 'react';
+import Filtros from './Filtros';
 
 export default function Estante(){
+
+  const[busca, setBusca] = useState("");
     return(
         <main>
         <nav className= {styles.jogos }>Estante da Locadora
         <img src={Logo} alt='Logo da Estante'/>
         </nav>
-        <header className={styles.header}></header>
+        <header className={styles.header}>
+            <div className={styles.header__text}>
+              "Locadora de todos os jogos"
+            </div>
+            </header>
+            <section className={styles.estante}>
+              <h3 className={styles.cardapio__titulo}>JOGOS</h3>
+              <Buscador busca={busca} setBusca={setBusca}/>
+            <div className={styles.estante__filtros}>
+            </div>
+            <Filtros/>
+
+
+            </section>
             </main>
     );
 }
